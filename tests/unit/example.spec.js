f87798f157f2fe44d8877e8ddc199ab4914a177b
Naomi Quinn <naomi.e.quinn@accenture.com>
2021-09-14 16:30:53 +0100

    added sample spec for signin

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,12 +1,71 @@
-import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
+import { shallowMount, createLocalVue  } from '@vue/test-utils'
+import Join from '../../src/views/Join.vue'
+import SignIn from '../../src/views/SignIn.vue'
+import Vuex from 'vuex';
+import Vuetify from 'vuetify'
 
-describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg }
+const localVue = createLocalVue()
+localVue.use(Vuex)
+
+describe('Auth', () => {
+  describe('Join', () => {
+    let actions
+    let store
+    let vuetify
+  
+    beforeEach(() => {
+      vuetify = new Vuetify(),
+      actions = {
+        userJoin: jest.fn(),
+      }
+      store = new Vuex.Store({
+        actions
+      })
     })
-    expect(wrapper.text()).toMatch(msg)
+  
+    it('dispatches "userJoin" login button is clicked', () => {
+      const joinStub = {
+        render: () => {},
+        methods: {
+          validate: () => true,
+        }
+      };
+      const wrapper = shallowMount(Join, { vuetify, store, localVue, stubs: {'v-form': joinStub}})
+      const button = wrapper.find('v-btn')
+      button.trigger('submit')
+      expect(actions.userJoin).toHaveBeenCalled()
+    })
   })
+  
+  describe('SignIn', () => {
+    let actions
+    let store
+    let vuetify
+  
+    beforeEach(() => {
+      vuetify = new Vuetify(),
+      actions = {
+        userLogin: jest.fn(),
+      }
+      store = new Vuex.Store({
+        actions
+      })
+    })
+  
+    it('dispatches "userLogin" login button is clicked', () => {
+      const signinStub = {
+        render: () => {},
+        methods: {
+          validate: () => true,
+        }
+      };
+      const wrapper = shallowMount(SignIn, { vuetify, store, localVue, stubs: {'v-form': signinStub}})
+      const button = wrapper.find('v-btn')
+      button.trigger('submit')
+      expect(actions.userLogin).toHaveBeenCalled()
+    })
+  })
+
 })
+
+
